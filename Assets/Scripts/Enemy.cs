16773c0f73bec8202da408647e2a134878b0e109
nosetatsuya <tatsudf33@icloud.com>
2024-07-16 22:04:38 +0900

    当たり判定の追加

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     public int gold;
     public Route route;
     public int pointIndex;
-
+    public bool hitBlock=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,90 +27,58 @@ public class Enemy : MonoBehaviour
             case 8:
                 v = route.points[pointIndex + 1].transform.position - route.points[pointIndex].transform.position;
                 transform.position += v.normalized * speed * Time.deltaTime;
+                var pv = transform.position - route.points[pointIndex].transform.position;
+                if (pv.magnitude >= v.magnitude)
+                {
+                    pointIndex++;
+                    if (pointIndex >= route.points.Length - 1)
+                    {
+                        if (gameObject.layer == 10) Debug.Log(gameObject);
+                        Destroy(gameObject);
+                        //ToDo プレイヤーにダメージ
+                        FindObjectOfType<Player>().hp--;
+                    }
+                }
                 break;
             case 10:
                 v = SearchEnemy();
                 transform.position += v.normalized * speed * Time.deltaTime;
+                if (transform.position == route.points[pointIndex+1].transform.position)
+                {
+                    pointIndex++;
+                    if (pointIndex >= route.points.Length - 1)
+                    {
+                        if (gameObject.layer == 10) Debug.Log(gameObject);
+                        Destroy(gameObject);
+                        //ToDo プレイヤーにダメージ
+                        FindObjectOfType<Player>().hp--;
+                    }
+                }
                 break;
-        }
-          
-
-        var pv = transform.position- route.points[pointIndex].transform.position;
-        if (pv.magnitude>=v.magnitude) {
-            pointIndex++;
-            if (pointIndex>=route.points.Length-1) {
-                if(gameObject.layer==10)Debug.Log(gameObject);
-                Destroy(gameObject);
-                //ToDo プレイヤーにダメージ
-                FindObjectOfType<Player>().hp--;
-            }
         }
     }
 
     private Vector3 SearchEnemy() {
         var retVector3 = new Vector3(0,0,0) ;
         var collider = Physics2D.OverlapCircle(transform.position, 4.0f, LayerMask.GetMask("Enemy"));
-        if (collider != null)
+        if (collider != null&&!hitBlock)
         {
             Enemy targetEnemy = collider.GetComponent<Enemy>();
             retVector3 = targetEnemy.route.points[targetEnemy.pointIndex + 1].transform.position - transform.position;
-            var collider_wall = Physics2D.OverlapBox(transform.position, new Vector2(0.0f,0.0f), LayerMask.GetMask("Block"));
-            if (collider_wall != null)
-            {
-
-                Debug.Log(collider_wall.GetComponent<Block>().transform.position);
-                //if (gameObject.layer == 10) Debug.Log(gameObject);
-                if (collider_wall.transform.position.x > transform.position.x && collider_wall.transform.position.x < transform.position.x + retVector3.normalized.x * speed * Time.deltaTime)
-                {
-                    retVector3.x = 0;
-                }
-                if (collider_wall.transform.position.x < transform.position.x && collider_wall.transform.position.x > transform.position.x + retVector3.normalized.x * speed * Time.deltaTime)
-                {
-                    retVector3.x = 0;
-                }
-                if (collider_wall.transform.position.y > transform.position.y && collider_wall.transform.position.y < transform.position.y + retVector3.normalized.y * speed * Time.deltaTime)
-                {
-                    retVector3.y = 0;
-                }
-                if (collider_wall.transform.position.y < transform.position.y && collider_wall.transform.position.y > transform.position.y + retVector3.normalized.y * speed * Time.deltaTime)
-                {
-                    retVector3.y = 0;
-                }
-            }
             
-            /*
-            RaycastHit[] _raycastHits = new RaycastHit[10];
-            // 自身とプレイヤーの座標差分を計算
-            var positionDiff = collider.transform.position - transform.position;
-
-            // プレイヤーとの距離を計算
-            var distance = positionDiff.magnitude;
-
-            // プレイヤーへの方向（長さだけ１にする処理。方向は変わらない）
-            var direction = positionDiff.normalized;
-
-            // _raycastHitsに、ヒットしたColliderや座標情報などが格納される
-            // RaycastAllとRaycastNonAllocは同等の機能だが、RaycastNonAllocだとメモリにゴミが残らないのでこちらを推奨
-            var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance);
-            if (hitCount > 0)
-            {
-                var diff = _raycastHits[0].transform.position - transform.position;
-                if (diff.magnitude<0.7f){
-                    if (diff.x<0.4f) {
-                        retVector3.x = 0;
-                    }
-                    if (diff.y < 0.4f)
-                    {
-                        retVector3.y= 0;
-                    }
-                }
-            }*/
         }
         else
         {
-            retVector3 = route.points[pointIndex + 1].transform.position - route.points[pointIndex].transform.position;
+            retVector3 = route.points[pointIndex + 1].transform.position - transform.position;
         }
         return retVector3;
 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Block")
+        {
+            hitBlock = true;
+        }
     }
 }
